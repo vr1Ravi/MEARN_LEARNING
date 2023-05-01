9ced1bec8fa028aa1dd7d0b4376638c21a8b3e75
Ravi <ravikumarjha059@gmail.com>
2023-05-01 09:44:13 -0700

    modified databse port

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-export const connectDatabase = ()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/myProtfolio").then((c) =>{
-        console.log(`MongoDb connected to:  ${c.connection.host}` );
-    }).catch((e)=>{
-        console.log(e);
+export const connectDatabase = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then((c) => {
+      console.log(`MongoDb connected to:  ${c.connection.host}`);
     })
-}
+    .catch((e) => {
+      console.log(e);
+    });
+};
