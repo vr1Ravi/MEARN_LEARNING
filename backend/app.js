c505dcbc652725efb051e57007718ebe3047c9f8
Ravi <ravikumarjha059@gmail.com>
2023-05-03 08:22:43 -0700

    commenting last two get method

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import { userRouter } from "./routes/User.js";
-import path from "path";
+// import path from "path";
 import dotenv from "dotenv";
 dotenv.config({
   path: "./backend/config/config.env",
@@ -13,9 +13,9 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" })); //middleware is 
 app.use(cookieParser());
 app.use("/api/v1", userRouter); //using Routes
 
-app.use(express.static(path.resolve("./frontend/build")));
+// app.use(express.static(path.resolve("./frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("./frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve("./frontend/build/index.html"));
+// });
 
