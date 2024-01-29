9155e1add6c13d417a278c6207a73dc48098e73f
Ravi <ravikumarjha059@gmail.com>
2024-01-29 15:26:10 +0530

    build added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,12 @@ function App() {
           height="80"
           width="80"
           ariaLabel="dna-loading"
-          wrapperStyle={{ position: "absolute", top: "50%", left: "50%" }}
+          wrapperStyle={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
           wrapperClass="dna-wrapper"
         />
       ) : (
